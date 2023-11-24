7a76e8f91dd72ee43c55f0f2b299ab8d6604792e
Gutemberg Ferreira Dantas <gutemberg.cz@gmail.com>
2023-11-24 10:23:31 -0300

    Refactoring connection with server

---
+++ Assets/Scripts/Server/ConfigClientConnection.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public class ConfigClientConnection
+    {
+        
+        public Socket Config()
+        {
+            var endPointConnection = new EndPointServer().GetEndPoint();
+
+            var socketConnection = new Socket(endPointConnection.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            return socketConnection;
+        }
+    }
+}
--- Assets/Scripts/Server/ConnectServer.cs
+++ Assets/Scripts/Server/ConnectServer.cs
@@ -11,70 +11,37 @@ using Interfaces;
 using Newtonsoft.Json;
 using Packets;
 using Serialization;
+using Server;
 using Unity.VisualScripting;
 using Utils;
 
 public class ConnectServer : MonoBehaviour
 {
-
-    //public string stateQuantityPlayers;
-    public static Text quantityPlayers;
-
-    private string HOST;
-    private int PORT;
-    private int BUFFER_LENGTH = 1024;
-
-    Socket socketConnection;
-
-    private PlayerMovement _playerMovement;
-
+    private Socket connection;
+    
     void Start()
     {
-        HOST = "localhost";
-        PORT = 11000;
-
-        ConnectClientOnServer();
-        _playerMovement = GameObject.FindObjectOfType<PlayerMovement>();
+        ConnectOnServer();
     }
 
-    void ConnectClientOnServer() 
+    void ConnectOnServer()
     {
+        var endPoint = new EndPointServer().GetEndPoint();
         
-        IPEndPoint EndPointConnection = ConfigEndPoint();
-
-        socketConnection = ConfigConnection(EndPointConnection);
-        socketConnection.Connect(EndPointConnection);
-
-        Debug.Log("Socket connected to >" 
-            + socketConnection.RemoteEndPoint.ToString());
-
-        Thread ListenerPackets =
-                new Thread(new ThreadStart(() => ListenPackets()));
-        ListenerPackets.Start();
-    }
-
-    IPEndPoint ConfigEndPoint()
-    {
-        IPHostEntry IpHost = Dns.GetHostEntry(HOST);
-        IPAddress IpAddress = IpHost.AddressList[0];
-        IPEndPoint EndPointConnection = new IPEndPoint(IpAddress, PORT);
-
-        return EndPointConnection;
-    }
-
-    Socket ConfigConnection(IPEndPoint EndPoint)
-    {
-        IPEndPoint EndPointConnection = EndPoint;
-
-        Socket SocketConnection = new Socket(EndPointConnection.AddressFamily,
+        connection = new Socket(endPoint.AddressFamily,
             SocketType.Stream, ProtocolType.Tcp);
+        connection = new ConfigClientConnection().Config();
+        connection.Connect(endPoint);
 
-        return SocketConnection;
+        Debug.Log("Connected to " + new ServerInfo().RemoteEndPoint(connection));
+
+        var listenerPackets = new Thread((ListenPackets));
+        listenerPackets.Start();
     }
 
-    void ListenPackets()
+    private void ListenPackets()
     {
-        var updateConnectedPlayers = new UpdateConnectedPlayers()
+        /*var updateConnectedPlayers = new UpdateConnectedPlayers()
         {
             opcode = 1,
             quantity = 0
@@ -83,55 +50,29 @@ public class ConnectServer : MonoBehaviour
         var packetToGetAllPlayerConnected =
             new SerializePacket().Serialize(updateConnectedPlayers);
         
-        socketConnection.Send(packetToGetAllPlayerConnected);
+        connection.Send(packetToGetAllPlayerConnected);*/
         
         while (true)
         {
-            var buffer = new byte[BUFFER_LENGTH];
-            var bytesReceived = socketConnection.Receive(buffer);
-            var packetReceived = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+            var buffer = new ServerInfo().GetBuffer();
+            var packetReceived = connection.Receive(buffer);
+            var packetSerialized = new PacketEncoding().String(packetReceived);
 
             var packets = new List<IPacketHandler>()
             {
-                new ConnectedPlayers(socketConnection),
-                new DisconnectPlayerPacket(socketConnection)
+                //new ConnectedPlayers(connection),
+                new DisconnectPlayerPacket(connection)
             };
 
             var packetHandler = new PacketManager(packets);
-            packetHandler.Manager(packetReceived);
-
-            /*if (packetReceived.Contains("\"opcode\":0"))
-            {
-                //Debug.Log("Server authorization to disconnect");
-                /*SocketConnection.Shutdown(SocketShutdown.Both);
-                SocketConnection.Close();
-            }
-            
-            if (packetReceived.Contains("\"opcode\":1"))
-            {
-                var updateConnectedPlayersPacket =
-                    new DeserializePacket().Deserialize<UpdateConnectedPlayers>(packetReceived);
-
-                Debug.Log("Quantity players received by server.");
-                stateQuantityPlayers = "" + updateConnectedPlayersPacket.quantity;
-            }
-
-            if (packetReceived.Contains("\"opcode\":2"))
-            {
-                var playerPositionPacket = 
-                    new DeserializePacket().Deserialize<PlayerPosition>(packetReceived);
-                Debug.Log(playerPositionPacket);
-            }*/
+            packetHandler.Manager(packetSerialized);
 
         }
     }
 
     void Update()
     {
-        //quantityPlayers.text = stateQuantityPlayers + " player(s) connected";
 
-        /*var playerPositionPacket = new SerializePacket().Serialize(_playerMovement.PlayerPosition());
-        SocketConnection.Send(playerPositionPacket);*/
     }
 
     private void OnApplicationQuit()
@@ -143,6 +84,6 @@ public class ConnectServer : MonoBehaviour
 
         var disconnectPlayerSerializedPacket = new SerializePacket().Serialize(disconnectPlayer);
         
-        socketConnection.Send(disconnectPlayerSerializedPacket);
+        connection.Send(disconnectPlayerSerializedPacket);
     }
 }
