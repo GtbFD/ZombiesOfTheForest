f3b09b075710be73d488fee0d779ca39ae8d19d1
Gutemberg Ferreira Dantas <gutemberg.cz@gmail.com>
2023-12-08 13:59:24 -0300

    fix(movementation): player's camera and your rotation

---
+++ Assets/Scripts/game/movement/camera/RotatePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace game.movement.camera
+{
+    public class RotatePlayer : MonoBehaviour
+    {
+
+        public Transform player;
+        private float mouseSens = 2f;
+        private float cameraVerticalRotation = 0f;
+
+        public GameObject target;
+        public void Update()
+        {
+            var inputX = Input.GetAxis("Mouse X") * mouseSens;
+            var inputY = Input.GetAxis("Mouse Y") * mouseSens;
+
+            cameraVerticalRotation -= inputY;
+            cameraVerticalRotation = Math.Clamp(cameraVerticalRotation, -90, 90);
+            transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+            
+            player.Rotate(Vector3.up * inputX);
+        }
+    }
+}
